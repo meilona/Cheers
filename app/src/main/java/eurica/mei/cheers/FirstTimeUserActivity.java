4e15ea29776cdc513dba7064982d111c6cba6111
wisuh <meilona.karmelia@student.umn.ac.id>
2020-05-13 23:26:21 +0700

    Front End First Time User

---
+++ app/src/main/java/eurica/mei/cheers/FirstTimeUserActivity.java
@@ -0,0 +1,14 @@
+package eurica.mei.cheers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstTimeUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_time_user);
+    }
+}
